a4e65c0b5b1d05e396e8e5b91320ff654dcaf7b5
matsuda0513 <gdluk4everythinguact@gmail.com>
2024-06-20 17:24:43 +0900

    投稿機能を実装

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -3,8 +3,22 @@ class PrototypesController < ApplicationController
   end
 
   def new
+    @prototype = Prototype.new
   end
 
   def create
+    @prototype = Prototype.new(prototype_params) 
+    if @prototype.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+private
+  def prototype_params
+    params.require(:prototype).permit(:title, :catch_copy, :cocept, :image).merge(user_id: current_user.id)
   end
 end
+
+# current_user.id は不要？
