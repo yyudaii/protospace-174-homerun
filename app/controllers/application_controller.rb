ac5beb16a6e0345247b93fb08ec751ad3d077f3e
matsuda0513 <gdluk4everythinguact@gmail.com>
2024-06-20 18:06:38 +0900

    ログインしていない場合はログイン画面に遷移

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+  # user管理機能マージ後以下必要？カラム名nameの確認要
+  # before_action :configure_permitted_parameters, if: :devise_controller?
+  # private
+  # def configure_permitted_parameters
+  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  # end
 end
