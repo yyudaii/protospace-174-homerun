592295eee0a55a1cd527ca8ff6b75906d7e7ab9b
yyudaii <yudai200009089311@gmail.com>
2024-06-20 14:01:42 +0900

    ルートの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
+  root to: new_user_registration_path
+  root to:  new_user_session_path
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
