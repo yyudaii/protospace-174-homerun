9b8f52d9eb42622a1097413b10d55be7ce54428b
yyudaii <yudai200009089311@gmail.com>
2024-06-21 21:23:51 +0900

    ローカルで実装済みのコメントアウトの削除

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -2,11 +2,10 @@ class Prototype < ApplicationRecord
   # belongs_to :room
   # belongs_to :user
   has_one_attached :image
+  belongs_to       :user
 
   validates :title, presence: true
   validates :catch_copy, presence: true
   validates :concept, presence: true
   validates :image, presence: true
-
-  belongs_to :user
 end
