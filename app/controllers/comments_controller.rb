b98d8e87f89ea416ad3e3dd0cc8dc81ee72e11c5
yyudaii <yudai200009089311@gmail.com>
2024-06-24 20:50:20 +0900

    軽微な修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,12 +6,13 @@ class CommentsController < ApplicationController
     else
       @prototype = @comment.prototype
       @comments = @prototype.comments
-      render = "prototypes/show"
+      render "prototypes/show"
     end
   end
 
   private
   def comment_params
+    params.require(:comment).permit(:text).merge(user_id: current_user.id, prototype_id: params[:prototype_id])
   end
 
 
