ab2d7c160f2341d91808d4064f3c9bb0aab01de7
matsuda0513 <gdluk4everythinguact@gmail.com>
2024-06-24 22:10:01 +0900

    ユーザー詳細画面に詳細と投稿一覧を表示させる

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,6 @@
 class UsersController < ApplicationController
   def show
-    @name = current_user.name
-    @profile = current_user.profile
-    @occupation = current_user.occupation
-    @position = current_user.position
+    @user = User.find(params[:id])
+    @prototype = @user.prototypes
   end
 end
