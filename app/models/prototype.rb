01ebfc140f8aa2bdf9829c6c3f6f9dc8a322ff60
yyudaii <yudai200009089311@gmail.com>
2024-06-24 17:08:05 +0900

    commentテーブルの作成、アソシエーションの記述

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -3,6 +3,7 @@ class Prototype < ApplicationRecord
   # belongs_to :user
   has_one_attached :image
   belongs_to       :user
+  has_many         :comments
 
   validates :title, presence: true
   validates :catch_copy, presence: true
