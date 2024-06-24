9ff7c48703e980ef993b75f5a134c01b68226d31
yyudaii <yudai200009089311@gmail.com>
2024-06-24 11:18:24 +0900

    アクションとルーティングの設定

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -20,6 +20,10 @@ class PrototypesController < ApplicationController
     @prototype = Prototype.find(params[:id])
   end
 
+  def destory
+
+  end
+
 private
   def prototype_params
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
